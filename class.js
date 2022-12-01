00e25c8ada03c629e50ac8566d0da6a6fedf1a25
Colin Marshall <colin.marshall94@gmail.com>
2022-11-30 16:24:23 -0800

    created classes in class.js

---
+++ class.js
@@ -0,0 +1,42 @@
+'use strict'
+
+class Employee {
+    constructor(name, id, email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+    getName() {}
+    getId() {}
+    getEmail() {}
+    getRole() {}
+}
+
+class Manager extends Employee {
+    constructor(name, id, email, officeNumber) {
+        super(name, id, email)
+        this.officeNumber = officeNumber;
+    }
+    //overrides Employee getRole() method
+    getRole() {}
+}
+
+class Engineer extends Employee {
+    constructor(name, id, email, github) {
+        super(name, id, email)
+        this.github = github;
+    }
+    getGithubb() {}
+    //overrides Employee getRole() method
+    getRole() {}
+}
+
+class Intern extends Employee {
+    constructor(name, id, email, school) {
+        super(name, id, email)
+        this.school = school;
+    }
+    getSchool() {}
+    //overrides Employee getRole() method
+    getRole() {}
+}
